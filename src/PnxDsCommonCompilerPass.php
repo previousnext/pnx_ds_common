ab88cd56643a51eb81d2baa7d25769027b538e65
Adam Bramley <adam.bramley@previousnext.com.au>
2025-05-06 12:24:12 +1000

    Fix Atom common components (#8)
    * Start fixing atoms
    
    * Fix common lib
    
    * Lint

--- src/PnxDsCommonCompilerPass.php
+++ src/PnxDsCommonCompilerPass.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Drupal\pnx_ds_common;
 
+use PreviousNext\Ds\Common\List\CommonAtoms;
 use PreviousNext\Ds\Common\List\CommonComponents;
 use PreviousNext\Ds\Common\List\CommonLayouts;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
@@ -16,6 +17,7 @@ final class PnxDsCommonCompilerPass implements CompilerPassInterface {
 
   public function process(ContainerBuilder $container): void {
     $pintoLists = $container->getParameter('pinto.lists');
+    $pintoLists[] = CommonAtoms::class;
     $pintoLists[] = CommonComponents::class;
     $pintoLists[] = CommonLayouts::class;
     $container->setParameter('pinto.lists', $pintoLists);
