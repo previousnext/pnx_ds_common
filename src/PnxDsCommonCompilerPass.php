b4c7b0f7985d5be1ae40ef17814262fe183c3bda
dpi <pro@danielph.in>
2025-06-02 17:14:53 +0800

    ✅ Testing Framework: regression testing, unit testing  (#13)

--- src/PnxDsCommonCompilerPass.php
+++ src/PnxDsCommonCompilerPass.php
@@ -4,9 +4,7 @@ declare(strict_types=1);
 
 namespace Drupal\pnx_ds_common;
 
-use PreviousNext\Ds\Common\List\CommonAtoms;
-use PreviousNext\Ds\Common\List\CommonComponents;
-use PreviousNext\Ds\Common\List\CommonLayouts;
+use PreviousNext\Ds\Common\List\CommonLists;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
@@ -17,9 +15,7 @@ final class PnxDsCommonCompilerPass implements CompilerPassInterface {
 
   public function process(ContainerBuilder $container): void {
     $pintoLists = $container->getParameter('pinto.lists');
-    $pintoLists[] = CommonAtoms::class;
-    $pintoLists[] = CommonComponents::class;
-    $pintoLists[] = CommonLayouts::class;
+    \array_push($pintoLists, ...CommonLists::Lists);
     $container->setParameter('pinto.lists', $pintoLists);
   }
 
