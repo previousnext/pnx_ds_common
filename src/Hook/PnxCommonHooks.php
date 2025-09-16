b1e818a461a5972ab441851f309203c13ae14f14
dpi <pro@danielph.in>
2025-09-16 11:04:07 +0800

    ⚗️⚰️ Remove experimental LB block bundles (#105)

--- src/Hook/PnxCommonHooks.php
+++ src/Hook/PnxCommonHooks.php
@@ -6,7 +6,6 @@ namespace Drupal\pnx_ds_common\Hook;
 
 use Drupal\Core\Extension\Extension;
 use Drupal\Core\Hook\Attribute\Hook;
-use PreviousNext\Ds\Common\BundleClass\Registry;
 use PreviousNext\Ds\Common\List\CommonComponents;
 use PreviousNext\Ds\Common\Utility;
 
@@ -28,21 +27,6 @@ final class PnxCommonHooks {
       // '/' indicates relative to DRUPAL_ROOT, not disk-root.
       $packageRoot = \realpath(\dirname($fileName) . '/..');
       $info['components']['namespaces'][Utility\Twig::NAMESPACE] = '/' . Utility\Twig::computePathFromDrupalRootTo($packageRoot);
-    }
-  }
-
-  /**
-   * Implements hook_entity_bundle_info_alter().
-   */
-  #[Hook('entity_bundle_info_alter')]
-  public function entityBundleInfoAlter(array &$bundles): void {
-    // @todo: figure out a way for us to not clobber when downstream projects need to extend bundle classes, because we need to allow that.
-    foreach (Registry::bundleClasses() as $bundleClass => $metadata) {
-      $entityType = $metadata->entityTypeId;
-      $bundle = $metadata->bundle;
-      if (isset($bundles[$entityType][$bundle])) {
-        $bundles[$entityType][$bundle]['class'] = $bundleClass;
-      }
     }
   }
 
