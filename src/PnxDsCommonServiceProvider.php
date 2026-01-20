a22fa9795edcbbdf03c37c274268f08a1bb96470
dpi <pro@danielph.in>
2026-01-20 14:53:30 +0800

    ✨ Collection capability repository: Discover which value types are produced when iterating or are appendable to collection objects (#108)

--- src/PnxDsCommonServiceProvider.php
+++ src/PnxDsCommonServiceProvider.php
@@ -6,13 +6,16 @@ namespace Drupal\pnx_ds_common;
 
 use Drupal\Core\DependencyInjection\ContainerBuilder;
 use Drupal\Core\DependencyInjection\ServiceProviderInterface;
+use PreviousNext\IdsTools\DependencyInjection\IdsCollectionGenericsCompilerPass;
 use PreviousNext\IdsTools\DependencyInjection\IdsModifierLookupCompilerPass;
 
 final class PnxDsCommonServiceProvider implements ServiceProviderInterface {
 
   public function register(ContainerBuilder $container): void {
-    $container->addCompilerPass(new PnxDsCommonCompilerPass(), priority: 100);
-    $container->addCompilerPass(new IdsModifierLookupCompilerPass());
+    $container
+      ->addCompilerPass(new PnxDsCommonCompilerPass(), priority: 100)
+      ->addCompilerPass(new IdsModifierLookupCompilerPass())
+      ->addCompilerPass(new IdsCollectionGenericsCompilerPass());
   }
 
 }
